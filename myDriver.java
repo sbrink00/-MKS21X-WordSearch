9a923d80fabb462ff3955e10f9c53569763b2d94
sbrink00 <sbrink00@stuy.edu>
2018-11-07 20:28:29 -0500

    although the driver looks short it had more test cases that I went through but then replaced with the final test cases shown here/

--- myDriver.java
+++ myDriver.java
@@ -1,7 +1,9 @@
 public class myDriver{
   public static void main(String[]args){
     WordSearch ws1 = new WordSearch(5, 5);
-    System.out.println(ws1.addWordHorizontally("four", 1, 1));
+    System.out.println(ws1.addWordHorizontally("four", 0, 0));
+    //System.out.println(ws1.addWordHorizontally("me", 0, 3));
+    System.out.println(ws1.addWordVertically("two", 2, 2));
     System.out.println(ws1);
   }
 }
