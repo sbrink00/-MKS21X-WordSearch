258f5507c651550baa699b9df536148ab8803428
sbrink00 <sbrink00@stuy.edu>
2018-11-08 21:04:02 -0500

    worte diagonal add method.

--- WordSearch.java
+++ WordSearch.java
@@ -33,17 +33,27 @@ public class WordSearch{
     boolean add = false;
     boolean conditional1 = false;
     boolean conditional2 = false;
+    boolean conditional3 = true;
     if (row >=0 && col >= 0 && row < data.length)  conditional1 = true;
     if (conditional1 && col <data[row].length && data[row].length - col >= word.length()) conditional2 = true;
-    if (conditional1 && conditional2) add = true;
+    if (conditional1 && conditional2){
+      int idx = col;
+      int wIdx = 0;
+      while (wIdx < word.length()){
+        if (data[row][idx] != '_' && data[row][idx] != word.charAt(wIdx)) conditional3 = false;
+        idx ++;
+        wIdx ++;
+      }
+    }
+    if (conditional1 && conditional2 && conditional3) add = true;
     if (add){
       int idx = col;
-      int wordIndex = 0;
-      while (wordIndex < word.length()){
-        if (data[row][idx] == '_') data[row][idx] = word.charAt(wordIndex);
+      int wIdx = 0;
+      while (wIdx < word.length()){
+        if (data[row][idx] == '_' || data[row][idx] == word.charAt(wIdx)) data[row][idx] = word.charAt(wIdx);
         else return false;
         idx ++;
-        wordIndex ++;
+        wIdx ++;
       }
       return true;
     }
@@ -54,21 +64,71 @@ public class WordSearch{
     boolean add = false;
     boolean conditional1 = false;
     boolean conditional2 = false;
+    boolean conditional3 = true;
     if (row >=0 && col >= 0 && row < data.length)  conditional1 = true;
     if (conditional1 && col <data[row].length && data.length - row >= word.length()) conditional2 = true;
-    if (conditional1 && conditional2) add = true;
+    if (conditional1 && conditional2){
+      int idx = row;
+      int wIdx = 0;
+      while (wIdx < word.length()){
+        if (data[idx][col] != '_' && data[idx][col] != word.charAt(wIdx)) conditional3 = false;
+        idx ++;
+        wIdx ++;
+      }
+    }
+    if (conditional1 && conditional2 && conditional3) add = true;
     if (add){
       int idx = row;
-      int wordIndex = 0;
-      while (wordIndex < word.length()){
-        if (data[idx][col] == '_') data[idx][col] = word.charAt(wordIndex);
+      int wIdx = 0;
+      while (wIdx < word.length()){
+        if (data[idx][col] == '_' || data[idx][col] == word.charAt(wIdx)){
+           data[idx][col] = word.charAt(wIdx);
+        }
         else return false;
         idx ++;
-        wordIndex ++;
+        wIdx ++;
       }
       return true;
     }
     return false;
   }
 
+  public boolean addWordDiagonal(String word, int row, int col){
+    boolean add = false;
+    boolean conditional1 = false;
+    boolean conditional2 = false;
+    boolean conditional3 = false;
+    boolean conditional4 = true;
+    if (row >=0 && col >= 0 && row < data.length)  conditional1 = true;
+    if (conditional1 && col < data[row].length) conditional2 = true;
+    if (conditional2 && data.length - row >= word.length() && data[row].length - col >= word.length()) conditional3 = true;
+    if (conditional1 && conditional2 && conditional3){
+      int idx = row;
+      int idx2 = col;
+      int wIdx = 0;
+      while (wIdx < word.length()){
+        if (data[idx][idx2] != '_' && data[idx][idx2] != word.charAt(wIdx)) conditional4 = false;
+        idx ++;
+        idx2 ++;
+        wIdx ++;
+      }
+    }
+    if (conditional1 && conditional2 && conditional3) add = true;
+    if (add){
+      int idx = row;
+      int idx2 = col;
+      int wIdx = 0;
+      while (wIdx < word.length()){
+        if (data[idx][idx2] == '_' || data[idx][idx2] == word.charAt(wIdx)){
+           data[idx][idx2] = word.charAt(wIdx);
+        }
+        else return false;
+        idx ++;
+        idx2 ++;
+        wIdx ++;
+      }
+      return true;
+    }
+    return false;
+  }
 }
