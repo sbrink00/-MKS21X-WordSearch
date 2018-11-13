1275d1a9df28f855ca4544073e50f8d7365823a5
sbrink00 <sbrink00@stuy.edu>
2018-11-12 21:37:00 -0500

    wordsearch is now in all caps and I wrote a funciton to fill in random letters, but I didn't implement that in the constructor.

--- WordSearch.java
+++ WordSearch.java
@@ -21,24 +21,13 @@ public class WordSearch{
         data[idx][idx2] = '_';
       }
     }
-    //File f = new File(fileName);
     Scanner in = new Scanner(new File(fileName));
-    while (in.hasNext()) wordsToAdd.add(in.next());
-    //System.out.println(wordsToAdd);
+    while (in.hasNext()) wordsToAdd.add(in.next().toUpperCase());
+    System.out.println(wordsToAdd);
     addAllWords();
   }
 
-  private void clear(){
-    for (int idx = 0; idx < data.length; idx ++){
-      for (int idx2 = 0; idx2 < data[idx].length; idx2 ++){
-        data[idx][idx2] = '_';
-      }
-    }
-  }
-
-  private boolean addAllWords(){
-    System.out.println(wordsToAdd);
-    System.out.println(wordsAdded);
+  private void addAllWords(){
     int counter = 0;
     while (!wordsToAdd.isEmpty() && counter < 50){
       boolean added = false;
@@ -47,7 +36,6 @@ public class WordSearch{
       String letters = wordsToAdd.get(randgen.nextInt(wordsToAdd.size()));
       System.out.println(letters);
       for (int idx = 0; idx < 100 & !added; idx ++){
-        System.out.println("idx: " + idx);
         int rIdx = randgen.nextInt(data.length);
         int cIdx = randgen.nextInt(data[0].length);
         if (addWord(letters, rIdx, cIdx, rInc, cInc)){
@@ -56,34 +44,10 @@ public class WordSearch{
         }
       }
       if (!added) counter ++;
-      System.out.println("counter: " + counter);
-      //System.out.println(addWord(letters, rIdx, cIdx, rInc, cInc));
     }
-    System.out.println(wordsToAdd);
-    System.out.println(wordsAdded);
-    return false;
   }
-    /*Random word = new Random();
-    Random ro = new Random();
-    Random c = new Random();
-    Random rd = new Random(2);
-    Random cd = new Random(3);
-    int counter = 0;
-    while (counter < 50 && !wordsToAdd.isEmpty()){
-      for (int idx = 0; idx < 100; idx ++){
-        String letters = wordsToAdd.get(word.nextInt(wordsToAdd.size()));
-        if (addWord(letters, ro.nextInt(data.length), c.nextInt(data[0].length), rd.nextInt() % 2, cd.nextInt() % 2)){
-          wordsAdded.add(letters);
-          wordsToAdd.remove(letters);
-          counter = 0;
-        }
-        else counter ++;
-      }
-    }
-    return false;*/
 
-
-  public boolean addWord(String word, int row, int col, int rowIncrement, int colIncrement){
+  private boolean addWord(String word, int row, int col, int rowIncrement, int colIncrement){
     if (rowIncrement == 0 && colIncrement == 0) return false;
     int rIdx = row;
     int cIdx = col;
@@ -113,10 +77,6 @@ public class WordSearch{
     else return false;
   }
 
-
-
-
-
   public String toString(){
     String output = "|";
     for (int idx = 0; idx < data.length; idx ++){
@@ -128,106 +88,25 @@ public class WordSearch{
     return output.substring(0, output.length() - 1);
   }
 
-  public boolean addWordHorizontal(String word, int row, int col){
-    boolean add = false;
-    boolean conditional1 = false;
-    boolean conditional2 = false;
-    boolean conditional3 = true;
-    if (row >=0 && col >= 0 && row < data.length)  conditional1 = true;
-    if (conditional1 && col <data[row].length && data[row].length - col >= word.length()) conditional2 = true;
-    if (conditional1 && conditional2){
-      int idx = col;
-      int wIdx = 0;
-      while (wIdx < word.length()){
-        if (data[row][idx] != '_' && data[row][idx] != word.charAt(wIdx)) conditional3 = false;
-        idx ++;
-        wIdx ++;
+  private void clear(){
+    for (int idx = 0; idx < data.length; idx ++){
+      for (int idx2 = 0; idx2 < data[idx].length; idx2 ++){
+        data[idx][idx2] = '_';
       }
     }
-    if (conditional1 && conditional2 && conditional3) add = true;
-    if (add){
-      int idx = col;
-      int wIdx = 0;
-      while (wIdx < word.length()){
-        if (data[row][idx] == '_' || data[row][idx] == word.charAt(wIdx)) data[row][idx] = word.charAt(wIdx);
-        else return false;
-        idx ++;
-        wIdx ++;
-      }
-      return true;
-    }
-    return false;
   }
 
-  public boolean addWordVertical(String word, int row, int col){
-    boolean add = false;
-    boolean conditional1 = false;
-    boolean conditional2 = false;
-    boolean conditional3 = true;
-    if (row >=0 && col >= 0 && row < data.length)  conditional1 = true;
-    if (conditional1 && col <data[row].length && data.length - row >= word.length()) conditional2 = true;
-    if (conditional1 && conditional2){
-      int idx = row;
-      int wIdx = 0;
-      while (wIdx < word.length()){
-        if (data[idx][col] != '_' && data[idx][col] != word.charAt(wIdx)) conditional3 = false;
-        idx ++;
-        wIdx ++;
+  //this method isn't going to be used yet,
+  //but eventually in order to have a completed
+  //wordsearch the rest of the slots need
+  //to be filled in with random letters.
+  private void fillInLetters(){
+    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (int rIdx = 0; rIdx < data.length; rIdx ++){
+      for (int cIdx = 0; cIdx < data[0].length; cIdx ++){
+        if (data[rIdx][cIdx] == '_') data[rIdx][cIdx] = alphabet.charAt(randgen.nextInt(26));
       }
     }
-    if (conditional1 && conditional2 && conditional3) add = true;
-    if (add){
-      int idx = row;
-      int wIdx = 0;
-      while (wIdx < word.length()){
-        if (data[idx][col] == '_' || data[idx][col] == word.charAt(wIdx)){
-           data[idx][col] = word.charAt(wIdx);
-        }
-        else return false;
-        idx ++;
-        wIdx ++;
-      }
-      return true;
-    }
-    return false;
   }
 
-  public boolean addWordDiagonal(String word, int row, int col){
-    boolean add = false;
-    boolean conditional1 = false;
-    boolean conditional2 = false;
-    boolean conditional3 = false;
-    boolean conditional4 = true;
-    if (row >=0 && col >= 0 && row < data.length)  conditional1 = true;
-    if (conditional1 && col < data[row].length) conditional2 = true;
-    if (conditional2 && data.length - row >= word.length() && data[row].length - col >= word.length()) conditional3 = true;
-    if (conditional1 && conditional2 && conditional3){
-      int idx = row;
-      int idx2 = col;
-      int wIdx = 0;
-      while (wIdx < word.length()){
-        if (data[idx][idx2] != '_' && data[idx][idx2] != word.charAt(wIdx)) conditional4 = false;
-        idx ++;
-        idx2 ++;
-        wIdx ++;
-      }
-    }
-    if (conditional1 && conditional2 && conditional3) add = true;
-    if (add){
-      int idx = row;
-      int idx2 = col;
-      int wIdx = 0;
-      while (wIdx < word.length()){
-        if (data[idx][idx2] == '_' || data[idx][idx2] == word.charAt(wIdx)){
-           data[idx][idx2] = word.charAt(wIdx);
-        }
-        else return false;
-        idx ++;
-        idx2 ++;
-        wIdx ++;
-      }
-      return true;
-    }
-    return false;
-  }
 }
