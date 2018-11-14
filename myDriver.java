0a7949bb7f8c6fa37dc6f35638d9578d1b84d18c
sbrink00 <sbrink00@stuy.edu>
2018-11-13 21:03:54 -0500

    driver that prints instructions if it is misused. The main from this driver will eventually be added to the wordsearch class.

--- myDriver.java
+++ myDriver.java
@@ -1,9 +1,25 @@
+import java.io.*;
 public class myDriver{
   public static void main(String[]args){
-    WordSearch ws1 = new WordSearch(5, 5);
-    System.out.println(ws1.addWordHorizontally("four", 0, 0));
-    //System.out.println(ws1.addWordHorizontally("me", 0, 3));
-    System.out.println(ws1.addWordVertically("two", 2, 2));
-    System.out.println(ws1);
+      String instructions = ""; //Displaying the answer key isn't coded yet, so the instructions will cover that later
+      instructions += "This program can be used with three or four arguments\n";
+      instructions += "3 arguments:\n arg 1 - number that represents number of rows";
+      instructions += "\n arg 2 - number that represents number of columns";
+      instructions += "\n arg 3 - string that is the file name you wish to use";
+      instructions += "\n4 arguments:\n the first three arguments are the same as when you are only using three arguments";
+      instructions += "\n arg 4 - number that will be a random seed so you can recreate the same wordsearch";
+      //note that if the terminal is too small the instructions won't be nicely formatted
+      WordSearch output;
+      if (args.length < 2 || args.length > 4) System.out.println(instructions);
+
+      if (args.length == 3){
+        output = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2]);
+        System.out.println(output);
+      }
+      if (args.length == 4){
+        output = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]));
+        System.out.println(output);
+      }
+
   }
 }
